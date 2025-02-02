7fefbfa4a5817716f3aa098b8b3e2e15474c552e
abhisek1221 <cherrymx317@gmail.com>
2025-02-02 22:48:34 +0530

    fetchPlaylistDetails

--- src/app/api/playlist/route.ts
+++ src/app/api/playlist/route.ts
@@ -1,4 +1,33 @@
 import { NextResponse } from "next/server";
+import { PlaylistDetails } from "@/lib/youtube";
+
+const YOUTUBE_API_BASE_URL = "https://www.googleapis.com/youtube/v3";
+const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
+async function fetchPlaylistDetails(playlistId: string): Promise<PlaylistDetails> {
+    const response = await fetch(
+      `${YOUTUBE_API_BASE_URL}/playlists?part=snippet&id=${playlistId}&key=${YOUTUBE_API_KEY}`
+    );
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  
+    const data = await response.json();
+    
+    const playlist = data.items[0];
+  
+    if (playlist === undefined) {
+      throw new Error("Invalid playlist ID");
+    }
+  
+    return {
+      id: playlist.id,
+      title: playlist.snippet.title,
+      description: playlist.snippet.description,
+      thumbnails: playlist.snippet.thumbnails,
+    };
+  }
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
