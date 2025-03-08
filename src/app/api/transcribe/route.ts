8ddb50a6945c2758800c710490b0f51b88d7b523
abhisek1221 <cherrymx317@gmail.com>
2025-03-08 15:53:23 +0530

    cv ui base setup

--- src/app/api/transcribe/route.ts
+++ src/app/api/transcribe/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
     const youtube = await Innertube.create({
       lang: 'en',
-      location: 'US',
+      location: 'IN',
       retrieve_player: false,
     })
 
