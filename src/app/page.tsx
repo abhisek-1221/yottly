d943178454dbb3622f365a4143fa6281d9e6736f
abhisek1221 <cherrymx317@gmail.com>
2025-05-16 11:02:05 +0530

    refactor: Merged standalone config of next.config.mjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { Suspense } from 'react'
-import { BackgroundBeamsDemo } from '@/components/Bgbeams'
-import { AnimatedGridPatternDemo } from '@/components/gp'
 import LandingPage from '@/components/LandingPage/lpg'
 
 export default function Home() {
